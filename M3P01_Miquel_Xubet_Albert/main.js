c6c831027feafab7fbb3131f5ed4123b8d8dd292
Miki <miquelxubet@gmail.com>
2024-07-02 00:12:31 +0200

    commit

---
+++ M3P01_Miquel_Xubet_Albert/main.js
@@ -0,0 +1,16 @@
+// Calcular el área del cuadrado
+// a = lado * lado
+
+const lado = 5;
+const areaCuadrado = lado * lado;
+
+console.log(`El Área del cuadrado es ${areaCuadrado}`);
+
+//Calcular el área del rectángulo
+// a = (base * altura ) / 2
+
+const base = 5;
+const altura = 12;
+const areaRectangulo = (base * altura) / 2;
+
+console.log(`El Área del rectángulo es ${areaRectangulo}`);
